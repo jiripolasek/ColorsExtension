96df96162915ce87b09d32e30b928b018ff7baa8
Jiří Polášek <me@jiripolasek.com>
2025-06-15 02:47:04 +0200

    Add human readable titles for error types

--- src/JPSoftworks.ColorsExtension/Pages/InputErrorListItem.cs
+++ src/JPSoftworks.ColorsExtension/Pages/InputErrorListItem.cs
@@ -14,8 +14,22 @@ internal sealed partial class InputErrorListItem : ListItem
 {
     public InputErrorListItem(string error, ParseErrorType type)
     {
-        this.Title = type.ToString();
+        this.Title = Format(type);
         this.Subtitle = error;
         this.Icon = Icons.Colorful.Error;
+    }
+
+    private static string Format(ParseErrorType type)
+    {
+        return type switch
+        {
+            ParseErrorType.UnknownSwitch => "Unknown switch",
+            ParseErrorType.MissingArgument => "Missing argument for switch",
+            ParseErrorType.UnexpectedArgument => "Unexpected argument",
+            ParseErrorType.InvalidValue => "Invalid value for switch",
+            ParseErrorType.MissingSwitchName => "Missing switch name",
+            ParseErrorType.InvalidSwitchName => "Invalid switch name",
+            _ => "Unknown error"
+        };
     }
 }
